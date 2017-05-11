062db385ad18b11ea1a38fef45574f76ac51e127
María José Rodríguez F <rodriguez.mariajose.f@gmail.com>
2017-05-11 16:40:53 -0300

    mas informacion

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,10 +7,24 @@ document.getElementById("toggle").addEventListener("click", function(event){
 var lastScrollTop = 0;
 window.addEventListener("scroll", function(){
 	var currentScroll = window.pageYOffset || document.body.scrollTop;
-	if (currentScroll < lastScrollTop){
+}; false);
+
+
+	if (currentScroll > lastScrollTop){
 		console.log("down");
 	} else{
 		console.log("up");
 	}
-    lastScrollTop = currentScroll:
-});
+    lastScrollTop = currentScroll;
+};
+
+}else{
+	document.getElementById("nav-header").classList.add("header-in");
+	document.getElementById("nav-header").classList.add("solid");
+	document.getElementById("nav-header").style.opacity = 1;
+	if (currentScroll<=3){
+		document.getElementById("nav-header").classList.remove("solid");
+		document.getElementById("nav-header").classList.remove("header-in");
+
+	}
+}
