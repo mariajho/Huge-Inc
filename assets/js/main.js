df65798db7b248cd1047086977b2f5215f9ec751
María José Rodríguez F <rodriguez.mariajose.f@gmail.com>
2017-05-11 23:47:44 -0300

    algunos cambios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-document.getElementById("toggle").addEventListener("click", function(event){
+document.getElementById("toggle").addListener("click", function event){
 	event.preventDefault();
 	document.getElementById("nav-header").classList.toggle("open");
 	document.getElementById("body").classList.toggle("overflow.hidden");
@@ -7,24 +7,22 @@ document.getElementById("toggle").addEventListener("click", function(event){
 var lastScrollTop = 0;
 window.addEventListener("scroll", function(){
 	var currentScroll = window.pageYOffset || document.body.scrollTop;
-}; false);
-
 
 	if (currentScroll > lastScrollTop){
-		console.log("down");
-	} else{
-		console.log("up");
-	}
-    lastScrollTop = currentScroll;
-};
-
-}else{
-	document.getElementById("nav-header").classList.add("header-in");
-	document.getElementById("nav-header").classList.add("solid");
-	document.getElementById("nav-header").style.opacity = 1;
-	if (currentScroll<=3){
+		document.getElementById("nav-header").classList.remove("header-in");
+		document.getElementById("nav-header").style.opacity = 0;
+	} 
+		
+	if (lastScrollTop=currentScroll){
 		document.getElementById("nav-header").classList.remove("solid");
 		document.getElementById("nav-header").classList.remove("header-in");
-
-	}
+		document.getElementById("nav-header").style.opacity=1;
 }
+	
+	if(currentScroll<3){
+		document.getElementById("nav-header").classList.remove("solid");
+		document.getElementById("nav-header").classList.remove("header-in");
+	}
+
+});	
+
